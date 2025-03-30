eec4c7707f243dee1909614b2da4c0316f11d6e5
Trey Cole <tbjcole@outlook.com>
2025-03-30 03:33:28 -0400

    fix header

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -11,13 +11,13 @@ export default (() => {
       <div>
         <div id="links-header">
           <span>
-            <a href="treycole.me">Home</a>
+            <a href="https://treycole.me">Home</a>
           </span>
           <span>
-            <a href="treycole.me/publications.html">Publications</a>
+            <a href="https://treycole.me/publications.html">Publications</a>
           </span>
           <span>
-            <a href="treycole.me/cv.pdf">CV</a>
+            <a href="https://treycole.me/cv.pdf">CV</a>
           </span>
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
