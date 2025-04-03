8aae8e9829613f1abb0abeefb9f3efd3fa9dc3d9
Trey Cole <tbjcole@outlook.com>
2025-04-03 15:05:06 -0400

    default datetype to modified

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "treycole.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
