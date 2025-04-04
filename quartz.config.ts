1caba67f16dce0ff1ff447775431b2ddc1feb171
Trey Cole <tbjcole@outlook.com>
2025-04-04 02:14:13 -0400

    sort notes by date created

--- quartz.config.ts
+++ quartz.config.ts
@@ -18,7 +18,7 @@ const config: QuartzConfig = {
     locale: "en-US",
     baseUrl: "treycole.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "modified",
+    defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
